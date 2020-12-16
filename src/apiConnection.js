60f376576955d4047caf12dc6d29a08d7633cee2
grzeskiewicz <benuch91@gmail.com>
2020-12-16 08:42:34 +0100

    Deploying to AWS

--- src/apiConnection.js
+++ src/apiConnection.js
@@ -1,4 +1,4 @@
-export const API_URL='http://localhost:8080';
+export const API_URL='https://timesheet-klaster.herokuapp.com';
 
 export const headers = new Headers({
     'Accept': 'application/json',
